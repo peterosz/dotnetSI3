c5558a02db1b103eddc94b6fa956653c9f4e9bf9
peter.osz <itharius@gmail.com>
2017-11-21 17:11:43 +0100

    uft-7 writer added

---
+++ ConvertTextFileToUTF7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertTextFileToUTF7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(@"C:\Users\ithar\Documents\test\test.txt"))
+            using (StreamWriter writer = new StreamWriter(@"C:\Users\ithar\Documents\test\test-utf7.txt", false, Encoding.UTF7))
+            writer.WriteLine(reader.ReadToEnd());
+        }
+    }
+}
