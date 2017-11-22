fbca4da63e08a8a2b917ba5a59762a00bd2f462a
peter.osz <itharius@gmail.com>
2017-11-22 13:30:47 +0100

    Exercise 2 done

--- SerializePeople/Person.cs
+++ SerializePeople/Person.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace SerializePeople
 {
@@ -15,15 +17,19 @@ namespace SerializePeople
         public DateTime BirthDate { get; set; }
         public Genders Gender { get; set; }
         public int Age;
+        static int counter;
+        static Dictionary<int,List<string>> people = new Dictionary<int, List<string>>();
 
         public Person() { }
 
         public Person(string Name, DateTime BirthDate, Genders Gender)
         {
+            counter++;
             this.Name = Name;
             this.BirthDate = BirthDate;
             this.Gender = Gender;
             Age = DateTime.Now.Year - BirthDate.Year;
+            people.Add(counter, new List<string> { Name, Age.ToString(), BirthDate.ToString(), Gender.ToString()});
         }
 
         public override string ToString()
@@ -32,6 +38,33 @@ namespace SerializePeople
                    "\n Age: " + Age +
                    "\n Date of Birth: " + BirthDate.ToString("yyyy/MM/dd") +
                    "\n Gender: " + Gender;
+        }
+
+        public static void Serialize()
+        {
+            string fileName = "SerializedPeople.txt";
+            FileInfo oldFile = new FileInfo("..\\" +fileName);
+            try
+            {
+                oldFile.Delete();
+            }
+            catch (FileNotFoundException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+             
+            using (FileStream newTextFile = File.Create(fileName))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                try
+                {
+                    formatter.Serialize(newTextFile, people);
+                }
+                catch (SerializationException ex)
+                {
+                    Console.WriteLine("Failed to serialize. Reason: " + ex.Message);
+                }
+            }
         }
     }
 }
--- SerializePeople/Program.cs
+++ SerializePeople/Program.cs
@@ -11,7 +11,9 @@ namespace SerializePeople
         static void Main(string[] args)
         {
             Person Peti = new Person("Peti", new DateTime(1986,06,30), Person.Genders.Male);
+            Person RandomGuy = new Person("RandomGuy", new DateTime(1911, 01, 21), Person.Genders.Male);
             Console.WriteLine(Peti);
+            Person.Serialize();
             Console.ReadKey();
         }
     }
