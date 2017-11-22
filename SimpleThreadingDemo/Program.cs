3911a58cab0de360966ab8e1ca37f8fe4a2e3597
peter.osz <itharius@gmail.com>
2017-11-22 18:56:59 +0100

    Exercise 1 done

---
+++ SimpleThreadingDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SimpleThreadingDemo
+{
+    class Program
+    {
+
+        static void Counting()
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                Console.WriteLine("Count: {0} - Thread Id: {1}", i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadStart starter = new ThreadStart(Counting);
+            Thread first = new Thread(starter);
+            Thread second = new Thread(starter);
+
+            first.Start();
+            second.Start();
+
+            first.Join();
+            second.Join();
+
+            Console.Read();
+        }
+    }
+}
