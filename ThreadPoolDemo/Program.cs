3de164705d2e35134015db882d4effa9291ee40c
peter.osz <itharius@gmail.com>
2017-11-23 13:51:52 +0100

    Exercise 3 done

---
+++ ThreadPoolDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static public void ShowMyText(object state)
+        {
+            string txt = "State: " + (string)state;
+            txt += " Thread: " + Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine(txt);
+        }
+
+        static void Main(string[] args)
+        {
+            WaitCallback callback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(callback, "Hello");
+            ThreadPool.QueueUserWorkItem(callback, "Hi");
+            ThreadPool.QueueUserWorkItem(callback, "Heya");
+            ThreadPool.QueueUserWorkItem(callback, "Goodbye");
+            Console.ReadKey();
+        }
+    }
+}
