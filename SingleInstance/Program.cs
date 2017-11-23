96d93118904303afedd054976889ca2c9245ee8d
peter.osz <itharius@gmail.com>
2017-11-23 13:47:27 +0100

    Exercise 2 done

---
+++ SingleInstance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SingleInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex mutex = null;
+            const string mutexName = "RUNMEONLYONCE";
+            while (true)
+            {
+                try
+                {
+                    mutex = Mutex.OpenExisting(mutexName);
+                    Console.WriteLine("Mutex found, exiting..");
+                    mutex.Close();
+                    break;
+                }
+                catch (WaitHandleCannotBeOpenedException)
+                {
+                    bool mutexIsMine = true;
+                    mutex = new Mutex(mutexIsMine, mutexName);
+                    Console.WriteLine("Mutex not found, created.");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
